9f2b88de1b2bc692c92dcfc322e4db5b3d39f8b3
Grzegorz Falkiewicz (Jelon) <95246683+GrzegorzFalkiewicz@users.noreply.github.com>
2024-12-11 17:56:20 +0100

    Update RegulatorPID.h

--- RegulatorPID.h
+++ RegulatorPID.h
@@ -2,8 +2,8 @@
 class RegulatorPID {
 private:
     double kp, ti, td;
-    double integralna;
-    double poprzedniaBladaTime, poprzedniaBladaWart;
+    double akumulator;
+    double poprzedniBladaWart;
 public:
     RegulatorPID(double kp, double ti = 0.0, double td = 0.0);
     double symuluj(double wejscie);
