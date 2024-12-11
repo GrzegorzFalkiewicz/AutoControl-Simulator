009c5664ef7e386aeb9ddba5ccf16cdb0de46032
Grzegorz Falkiewicz (Jelon) <95246683+GrzegorzFalkiewicz@users.noreply.github.com>
2024-12-11 17:54:50 +0100

    Update RegulatorPID.cpp

--- RegulatorPID.cpp
+++ RegulatorPID.cpp
@@ -2,38 +2,36 @@
 #include <iostream>
 
 RegulatorPID::RegulatorPID(double kp, double ti, double td)
-    : kp(kp), ti(ti), td(td), integralna(0.0), poprzedniaBladaTime(0.0), poprzedniaBladaWart(0.0) {}
+    : kp(kp), ti(ti), td(td), akumulator(0.0), poprzedniBladaWart(0.0) {}
 
 double RegulatorPID::symuluj(double wejscie) {
     double blad = wejscie;
-    double czasDelta = 1; // przyjmujemy, że próbkowanie odbywa się z częstotliwością 1 Hz
+    double roznicaCzasu = 1.0; // 1Hz
 
     // Człon proporcjonalny
     double wyj_p = kp * blad;
 
-    // Człon całkujący
+    // Człon calkujący
     double wyj_i = 0.0;
     if (ti != 0.0) {
-        integralna += blad * czasDelta;
-        wyj_i = integralna * kp / ti;
+        akumulator += blad * roznicaCzasu; //akumulacja bledu
+        wyj_i = akumulator * (kp / ti);
     }
 
-    // Człon różniczkujący
+    // Człon rozniczkujący
     double wyj_d = 0.0;
-    if (td != 0.0) {
-        wyj_d = kp * td * (blad - poprzedniaBladaWart) / czasDelta ;
-        poprzedniaBladaWart = blad;
-    }
+        wyj_d = kp * td * (blad - poprzedniBladaWart) / roznicaCzasu;
+        poprzedniBladaWart = blad;
 
     // Wynik sumaryczny
     double wyj = wyj_p + wyj_i + wyj_d;
 
-    // Logowanie wartości do debugowania
-   /* std::cout << "blad: " << blad
+    // Wartosci do debugowania
+    std::cout << std::endl << "blad: " << blad
         << ", wyj_p: " << wyj_p
         << ", wyj_i: " << wyj_i
         << ", wyj_d: " << wyj_d
-        << ", wyj: " << wyj << std::endl;*/
+        << ", wyj: " << wyj << std::endl;
 
     return wyj;
 }
