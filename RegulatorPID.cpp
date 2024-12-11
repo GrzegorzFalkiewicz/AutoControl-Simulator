33b8017b111b1c526c1ed574a2d3cbcf63f70c9a
Grzegorz Falkiewicz (Jelon) <95246683+GrzegorzFalkiewicz@users.noreply.github.com>
2024-12-11 17:10:06 +0100

    Update RegulatorPID.cpp

--- RegulatorPID.cpp
+++ RegulatorPID.cpp
@@ -6,30 +6,29 @@ RegulatorPID::RegulatorPID(double kp, double ti, double td)
 
 double RegulatorPID::symuluj(double wejscie) {
     double blad = wejscie;
-    double czasDelta = 1; // przyjmujemy, ¿e próbkowanie odbywa siê z czêstotliwoœci¹ 1 Hz
+    double czasDelta = 1; // przyjmujemy, Å¼e prÃ³bkowanie odbywa siÄ™ z czÄ™stotliwoÅ›ciÄ… 1 Hz
 
-    // Cz³on proporcjonalny
+    // CzÅ‚on proporcjonalny
     double wyj_p = kp * blad;
 
-    // Cz³on ca³kuj¹cy
+    // CzÅ‚on caÅ‚kujÄ…cy
     double wyj_i = 0.0;
     if (ti != 0.0) {
         integralna += blad * czasDelta;
         wyj_i = integralna * kp / ti;
     }
 
-    // Cz³on ró¿niczkuj¹cy
+    // CzÅ‚on rÃ³Å¼niczkujÄ…cy
     double wyj_d = 0.0;
     if (td != 0.0) {
-        wyj_d = kp * td * (blad - poprzedniaBladaWart) / (czasDelta + poprzedniaBladaTime);
+        wyj_d = kp * td * (blad - poprzedniaBladaWart) / czasDelta ;
         poprzedniaBladaWart = blad;
-        poprzedniaBladaTime = czasDelta;
     }
 
     // Wynik sumaryczny
     double wyj = wyj_p + wyj_i + wyj_d;
 
-    // Logowanie wartoœci do debugowania
+    // Logowanie wartoÅ›ci do debugowania
    /* std::cout << "blad: " << blad
         << ", wyj_p: " << wyj_p
         << ", wyj_i: " << wyj_i
